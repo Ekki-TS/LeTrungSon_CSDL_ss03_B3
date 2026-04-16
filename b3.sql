34cc503653f0494b633bce61d6a9725067006284
Ekki-TS <trungsonle32@gmail.com>
2026-04-16 20:31:02 +0700

    Update b3.sql

--- b3.sql
+++ b3.sql
@@ -15,16 +15,24 @@ CREATE TABLE CUSTOMERS (
 
 INSERT INTO CUSTOMERS (FullName, Email, City, LastPurchaseDate, Status)
 VALUES
-(N'Nguyễn Văn A', 'anv@gmail.com', N'Hà Nội', '2025-05-20', 'Active'),
-(N'Trần Thị B', 'btt@gmail.com', N'Hà Nội', '2026-02-10', 'Active'),
-(N'Lê Văn C', NULL, N'Hà Nội', '2025-01-15', 'Active'),
-(N'Phạm Minh D', 'dpm@gmail.com', N'Hà Nội', '2024-12-01', 'Locked'),
-(N'Hoàng An E', 'eha@gmail.com', N'TP HCM', '2025-03-01', 'Active');
+('Nguyễn Văn A', 'anv@gmail.com', 'Hà Nội', '2025-05-20', 'Active'),
+('Trần Thị B', 'btt@gmail.com', 'Hà Nội', '2026-02-10', 'Active'),
+('Lê Văn C', NULL, 'Hà Nội', '2025-01-15', 'Active'),
+('Phạm Minh D', 'dpm@gmail.com', 'Hà Nội', '2024-12-01', 'Locked'),
+('Hoàng An E', 'eha@gmail.com', 'TP HCM', '2025-03-01', 'Active');
 
-SELECT CustomerID, FullName, Email, City, LastPurchaseDate, Status FROM CUSTOMERS
+SELECT 
+    CustomerID, 
+    FullName, 
+    Email, 
+    City, 
+    LastPurchaseDate, 
+    Status 
+FROM CUSTOMERS
 WHERE 
-    (City = N'Hà Nội')
+    City = 'Hà Nội'
     AND Email IS NOT NULL 
     AND Email != ''
     AND Status = 'Active'
-    AND LastPurchaseDate < DATE_SUB(NOW(), INTERVAL 6 MONTH);
+    AND LastPurchaseDate < '2026-04-01'
+ORDER BY LastPurchaseDate DESC;
